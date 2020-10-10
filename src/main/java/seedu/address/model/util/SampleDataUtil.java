f15a4f42cbf2e95114caa6395495e7e985e0ed64
duynguyen24501 <duynguyen24501@gmail.com>
2020-10-10 12:13:36 +0800

    Fix sample data to correctly initialize storage json file

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -18,24 +18,12 @@ import seedu.address.model.tag.Tag;
 public class SampleDataUtil {
     public static Bug[] getSampleBugs() {
         return new Bug[] {
-            new Bug(new Name("Alex Yeoh"), new State("alexyeoh@example.com"),
-                new Description("Blk 30 Geylang Street 29, #06-40"),
-                getTagSet("friends")),
-            new Bug(new Name("Bernice Yu"), new State("berniceyu@example.com"),
-                new Description("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                getTagSet("colleagues", "friends")),
-            new Bug(new Name("Charlotte Oliveiro"), new State("charlotte@example.com"),
-                new Description("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                getTagSet("neighbours")),
-            new Bug(new Name("David Li"), new State("lidavid@example.com"),
-                new Description("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                getTagSet("family")),
-            new Bug(new Name("Irfan Ibrahim"), new State("irfan@example.com"),
-                new Description("Blk 47 Tampines Street 20, #17-35"),
-                getTagSet("classmates")),
-            new Bug(new Name("Roy Balakrishnan"), new State("royb@example.com"),
-                new Description("Blk 45 Aljunied Street 85, #11-31"),
-                getTagSet("colleagues"))
+            new Bug(new Name("Bug 1"), new State("backlog"),
+                new Description("Description of bug 1"),
+                getTagSet("JavaFX")),
+            new Bug(new Name("Bug 2"), new State("backlog"),
+                new Description("Description of bug 2"),
+                getTagSet("JavaFX", "frontend")),
         };
     }
 
